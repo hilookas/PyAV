3c455601c9be687eed666a1a5186897d3cb26431
lookas <i@18kas.com>
2025-08-30 13:04:03 +0800

    Update scripts/activate.sh

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -50,17 +50,17 @@ print("{}{}.{}".format(platform.python_implementation().lower(), *sys.version_in
     ')"
     export PYAV_VENV="$PYAV_ROOT/venvs/$PYAV_VENV_NAME"
 
-    if [[ ! -e "$PYAV_VENV/local/bin/python" ]]; then
+    if [[ ! -e "$PYAV_VENV/bin/python" ]]; then
         mkdir -p "$PYAV_VENV"
         virtualenv -p "$PYAV_PYTHON" "$PYAV_VENV"
-        "$PYAV_VENV/local/bin/pip" install --upgrade pip setuptools
+        "$PYAV_VENV/bin/pip" install --upgrade pip setuptools
     fi
 
-    if [[ -e "$PYAV_VENV/local/bin/activate" ]]; then
-        source "$PYAV_VENV/local/bin/activate"
+    if [[ -e "$PYAV_VENV/bin/activate" ]]; then
+        source "$PYAV_VENV/bin/activate"
     else
         # Not a virtualenv (perhaps a debug Python); lets manually "activate" it.
-        PATH="$PYAV_VENV/local/bin:$PATH"
+        PATH="$PYAV_VENV/bin:$PATH"
     fi
 
 fi
